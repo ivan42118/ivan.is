c71a984677ef312e3eacedd29314d78311345e12
ivan42118 <ivan42118@gmail.com>
2024-08-18 01:30:01 +0700

    updt

--- js/chunk-2d0e264d.ccdf0003.js
+++ js/chunk-2d0e264d.ccdf0003.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e264d"],{"7f08":function(n,u,c){"use strict";c.r(u);var d=c("f933");c.d(u,"default",(function(){return d["a"]}))}}]);
-=======
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e264d"],{"7f08":function(n,u,c){"use strict";c.r(u);var d=c("f933");c.d(u,"default",(function(){return d["a"]}))}}]);
->>>>>>> 2ab6562ec0b85f869ed179086222c08044f0c79b
 //# sourceMappingURL=chunk-2d0e264d.ccdf0003.js.map
