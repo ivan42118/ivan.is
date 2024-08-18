49fdd6ed51156484c43c8390b254605ba90e7e07
ivan42118 <ivan42118@gmail.com>
2024-08-18 12:29:16 +0700

    about link

--- js/app.be460178.js
+++ js/app.be460178.js
@@ -4194,7 +4194,7 @@
             },
           },
           {
-            path: "ivan.is/about",
+            path: "/about",
             name: ".about()",
             component: function () {
               return n.e("chunk-0294ec15").then(n.bind(null, "f820"));
